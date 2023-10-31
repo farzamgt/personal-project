53d3a267e1943b2e5793167187b7522396c7f0b1
Farzam Tehrani <farzamgt@gmail.com>
2023-10-31 19:39:24 +0200

    changes for some ditals implemented

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.querySelector(".burger-icon");
-    const navBurger = document.querySelector(".nav__burger");
-    const xIcon = document.querySelector(".x-icon");
+// Burger menu
+const menuIcon = document.querySelector(".burger-icon");
+const navBurger = document.querySelector(".nav__burger");
+const xIcon = document.querySelector(".x-icon");
 
-    function openMenu() {
-        navBurger.style.display = "grid";
-        menuIcon.style.display = "none";
-    }
+function openMenu() {
+    navBurger.style.display = "grid";
+    menuIcon.style.display = "none";
+}
 
-    function closeMenu() {
-        navBurger.style.display = "none";
-        menuIcon.style.display = "flex";
-    }
+function closeMenu() {
+    navBurger.style.display = "none";
+    menuIcon.style.display = "flex";   
+}
 
-    menuIcon.addEventListener("click", openMenu);
-    xIcon.addEventListener("click", closeMenu);
-});
+menuIcon.addEventListener("click", openMenu);
+xIcon.addEventListener("click", closeMenu);
