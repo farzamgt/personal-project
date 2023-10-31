10181d0e3fa7968f05b2aef65db6f8e38e2c9703
Farzam Tehrani <farzamgt@gmail.com>
2023-10-31 18:39:30 +0200

    js file for burger menu implemented

---
+++ js/index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".burger-icon");
+    const navBurger = document.querySelector(".nav__burger");
+    const xIcon = document.querySelector(".x-icon");
+
+    function openMenu() {
+        navBurger.style.display = "grid";
+        menuIcon.style.display = "none";
+    }
+
+    function closeMenu() {
+        navBurger.style.display = "none";
+        menuIcon.style.display = "flex";
+    }
+
+    menuIcon.addEventListener("click", openMenu);
+    xIcon.addEventListener("click", closeMenu);
+});
