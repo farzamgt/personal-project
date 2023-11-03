6f138c1dbf79e32c54e7cabe35df581a9a5ce52b
Farzam Tehrani <farzamgt@gmail.com>
2023-11-03 17:05:39 +0200

    implemented carousel and corrected styles

--- js/index.js
+++ js/index.js
@@ -13,4 +13,38 @@ toggleBtn.onclick = function () {
 };
 
 // Carousel
+const carousel = document.querySelector(".tender__links_card");
+const carouselBtns = document.querySelectorAll(".carousel_btn i");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth + 20;
+
+let isDragging = false, startX, startScrollLeft;
+
+carouselBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+    })
+});
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragging) return;
+    carousel.scrollLeft = startScrollLeft -(e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+
+
 
